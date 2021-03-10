dbce9d025b9e06ed75625228d47aa36d1f917dc1
Dean Stuart <dean.stuart@uvm.edu>
2021-03-10 11:29:30 -0500

    initialize player class

--- player.h
+++ player.h
@@ -5,6 +5,33 @@
 #ifndef MONOPLOY_PLAYER_H
 #define MONOPLOY_PLAYER_H
 
+#include <string>
 
+class Player {
+protected:
+    int account;
+    string name;
+    int tile;
+public:
+    // constructor
+    Player(string name);
+
+    // destructor
+    virtual ~Player() = default;
+
+    // getters
+    int getAccount();
+    string getName();
+    int getTile();
+
+    // setters
+    void setAccount(int amount);
+    void setTile(int tileNum);
+
+    // class functions
+    void addFunds(int amount);
+    void subtractFunds(int amount);
+
+};
 
 #endif //MONOPLOY_PLAYER_H
