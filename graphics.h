3e9daccdf6030be0fabc78e45866975cf1c5d65d
Dean Stuart <dean.stuart@uvm.edu>
2021-03-10 11:21:27 -0500

    initial commit

---
+++ graphics.h
@@ -0,0 +1,8 @@
+//
+// Created by deanp on 3/10/2021.
+//
+
+#ifndef MONOPLOY_GRAPHICS_H
+#define MONOPLOY_GRAPHICS_H
+
+#endif //MONOPLOY_GRAPHICS_H
---
+++ player.h
@@ -0,0 +1,10 @@
+//
+// Created by deanp on 3/10/2021.
+//
+
+#ifndef MONOPLOY_PLAYER_H
+#define MONOPLOY_PLAYER_H
+
+
+
+#endif //MONOPLOY_PLAYER_H
